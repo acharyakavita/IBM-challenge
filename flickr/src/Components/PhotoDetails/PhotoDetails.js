16eb61c548d867a42ca5130533b053c45a305639
acharyakavita <acharyakavita12@gmail.com>
2020-03-16 09:28:09 -0400

    Fix for date upload

--- flickr/src/Components/PhotoDetails/PhotoDetails.js
+++ flickr/src/Components/PhotoDetails/PhotoDetails.js
@@ -8,7 +8,7 @@ import Header from '../Header/Header'
 const PhotoDetails = props=> {
 
     let photo = props.location.state.detail;
-    let date=new Date(parseInt(photo.dateupload))
+    let date=new Date(parseInt(photo.dateupload)*1000)
     let formattedDate=date.getUTCDate() + '/' + (date.getUTCMonth() + 1)+ '/' + date.getUTCFullYear();
     
     return(
